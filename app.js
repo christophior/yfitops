5e7abfa9ff3f8929992e15f4f8177735712e262a
christophior <chrisvillarreal1018@gmail.com>
2015-07-18 16:50:14 -0500

    Added ability to download starred playlists, changed playlist implementation a bit

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ var config = require('./config'),
 
 if (process.argv.length >= 3) {
     Spotify.login(username, password, function (err, spotify) {
-        if (err) {
-            throw err;
-        }
+        if (err) throw err;
 
-        var uriType = uri.split(':');
+        var uriType = Spotify.uriType(uri);
 
-        if (uriType[1] === 'track') {
+        if (uriType === 'track') {
             console.log('Given URI is a Spotify track...');
             download.track(spotify, uri, function (err) {
                 spotify.disconnect();
             });
-        } else if (uriType[3] === 'playlist') {
+        } else if (uriType === 'playlist') {
             console.log('Given URI is a Spotify playlist...');
-            download.playlist(spotify, uri, function (err) {
-                spotify.disconnect();
-            });
+            var starredPlaylistUsername = getStarredPlaylistUsername(uri);
+            if (starredPlaylistUsername){
+                download.starredPlaylist(spotify, starredPlaylistUsername, disconnect);
+            } else {
+                download.playlist(spotify, uri, disconnect);
+            }
         } else {
             console.log('Invalid URI given');
         }
@@ -32,3 +33,15 @@ if (process.argv.length >= 3) {
 } else {
     console.log('Please pass in a Spotify URI');
 }
+
+var getStarredPlaylistUsername = function (playlistURI) {
+    var splitURI = playlistURI.split(':');
+    var isStarred = splitURI[3] === 'starred' ? true : false;
+    console.log(isStarred);
+    console.log(splitURI[2]);
+    return isStarred ? splitURI[2] : undefined;
+};
+
+var disconnect = function (spotify) {
+    spotify.disconnect();
+};
